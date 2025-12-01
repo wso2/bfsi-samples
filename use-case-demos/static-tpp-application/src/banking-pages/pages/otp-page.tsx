e5e77de1c3110bb2d34db9f9c9f037e08579a67f
lasithLRI <lasith.hettiarachchi.dev@gmail.com>
2025-12-01 11:31:01 +0530

    OTP validation removed from banking pages

--- use-case-demos/static-tpp-application/src/banking-pages/pages/otp-page.tsx
+++ use-case-demos/static-tpp-application/src/banking-pages/pages/otp-page.tsx
@@ -46,7 +46,7 @@ const OtpPage = ()=>{
         }
     })
     const onSubmitHandler=(data:OtpFormData)=>{
-        if(data.code==='55555'){
+        if(data.code){
             onSuccessHandler()
         }else{
             alert("Check your Otp and re-enter")
